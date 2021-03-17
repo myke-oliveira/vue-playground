25b158a72e13991912557896bfbcd6448133cea3
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2021-03-17 09:14:31 -0300

    4º Exemplo

--- index.js
+++ index.js
@@ -10,11 +10,22 @@ const app2 = new Vue({
   data: {
     message: 'Você carregou esta página em ' + new Date().toLocaleString(),
   }
-})
+});
 
 const app3 = new Vue({
   el: '#app-3',
   data: {
     seen: true,
+  }
+});
+
+const app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'Aprender JavaScript' },
+      { text: 'Aprender Vue' },
+      { text: 'Criar algo incrível' }
+    ]
   }
 })
