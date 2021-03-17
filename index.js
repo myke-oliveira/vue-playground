d28132bbeb8c11c9ca5e990af9ba98e70f2fdaaa
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2021-03-17 09:10:33 -0300

    3 examples

---
+++ index.js
@@ -0,0 +1,20 @@
+const app1 = new Vue({
+  el: '#app-1',
+  data: {
+    message: 'Olá vue!'
+  }
+});
+
+const app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'Você carregou esta página em ' + new Date().toLocaleString(),
+  }
+})
+
+const app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen: true,
+  }
+})
